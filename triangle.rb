e9e7cfc6991031a7b56d2cba8ec36f122f7abca4
naoki saruta <katsusand1@gmail.com>
2020-03-01 10:52:19 +0900

    Add cases for triangle with unequally long three sides

--- triangle.rb
+++ triangle.rb
@@ -2,6 +2,7 @@ class Triangle
   attr_accessor :a, :b, :c
 
   def equilateral_triangle?
-    true
+    return false unless [@a, @b, @c].all?{ |side| side.is_a?(Numeric) && side > 0 }
+    @a == @b && @a == @c && @b == @c
   end
 end
--- triangle_spec.rb
+++ triangle_spec.rb
@@ -2,10 +2,26 @@ require_relative 'triangle'
 
 describe Triangle do
   context 'with three equally long sides' do
-    @triangle = Triangle.new
-    @triangle.a = 1
-    @triangle.b = 1
-    @triangle.c = 1
-    it { is_expected.to be_equilateral_triangle }
+    triangle = Triangle.new
+    triangle.a = 1
+    triangle.b = 1
+    triangle.c = 1
+    it { expect(triangle).to be_equilateral_triangle }
+  end
+
+  context 'with unequally long three sides' do
+    triangle = Triangle.new
+    triangle.a = 3
+    triangle.b = 4
+    triangle.c = 5
+    it { expect(triangle).to_not be_equilateral_triangle }
+  end
+
+  context 'has nil for one of side lengths' do
+    triangle = Triangle.new
+    triangle.a = 3
+    triangle.b = 4
+    triangle.c = nil
+    it { expect(triangle).to_not be_equilateral_triangle }
   end
 end
