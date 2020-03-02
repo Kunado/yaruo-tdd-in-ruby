ec94f3051b8ff997dc000820151e47035d355f13
naoki saruta <katsusand1@gmail.com>
2020-03-02 20:33:07 +0900

    Define private method to check if side lengths are positive numerical values

--- triangle.rb
+++ triangle.rb
@@ -2,7 +2,12 @@ class Triangle
   attr_accessor :a, :b, :c
 
   def equilateral_triangle?
-    return false unless [@a, @b, @c].all?{ |side| side.is_a?(Numeric) && side > 0 }
+    return false unless positive_values?
     @a == @b && @a == @c && @b == @c
   end
+
+  private
+    def positive_values?
+      [@a, @b, @c].all?{ |side| side.is_a?(Numeric) && side > 0 }
+    end
 end
