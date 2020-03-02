367a25cfc9dd1e9142fc0750b5657e35426b4ea0
naoki saruta <katsusand1@gmail.com>
2020-03-02 21:49:21 +0900

    Add private method to check if given line lengths satisfy condition to be triangle

--- triangle.rb
+++ triangle.rb
@@ -2,17 +2,22 @@ class Triangle
   attr_accessor :a, :b, :c
 
   def equilateral_triangle?
-    return false unless positive_values?
+    return false unless triangle?
     @a == @b && @a == @c && @b == @c
   end
 
   def isosceles_triangle?
-    return false unless positive_values?
+    return false unless triangle?
     @a == @b || @a == @c || @b == @c
   end
 
   private
     def positive_values?
       [@a, @b, @c].all?{ |side| side.is_a?(Numeric) && side > 0 }
+    end
+
+    def triangle?
+      return false unless positive_values?
+      @a + @b > @c && @a + @c > @b && @b + @c > @a
     end
 end
--- triangle_spec.rb
+++ triangle_spec.rb
@@ -27,6 +27,14 @@ describe Triangle do
     it { expect(triangle).to_not be_isosceles_triangle }
   end
 
+  context 'with two equally long sides that cannot be triangle' do
+    triangle = Triangle.new
+    triangle.a = 2
+    triangle.b = 1
+    triangle.c = 1
+    it { expect(triangle).to_not be_isosceles_triangle }
+  end
+
   context 'has nil for one of side lengths' do
     triangle = Triangle.new
     triangle.a = 3
