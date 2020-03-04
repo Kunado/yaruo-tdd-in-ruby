0dcafca06633e736780ea8b0f3cbdbcfba21aa45
naoki saruta <katsusand1@gmail.com>
2020-03-04 19:25:23 +0900

    Add specs and simple implementation which passes those specs

--- csv_parser.rb
+++ csv_parser.rb
@@ -1,5 +1,35 @@
 class CSVParser
-  def self.parse(text)
-    text.split(',')
+  def self.parse(text, array = [])
+    return if text.length == 0
+    c = text.slice!(0)
+    if c == '"'
+      parse_double_quote text, array
+    else
+      array.push c if c != ',' && c != ' '
+      parse text, array
+    end
+    array
+  end
+
+  def self.parse_double_quote(text, array = [], string = '')
+    return if text.length == 0
+    c = text.slice!(0)
+    if c == '"'
+      parse_double_quote_in_double_quote text, array, string
+    else
+      string += c
+      parse_double_quote text, array, string
+    end
+  end
+
+  def self.parse_double_quote_in_double_quote(text, array = [], string = '')
+    c = text.slice!(0)
+    if c == '"'
+      string += c
+      parse_double_quote text, array, string
+    else
+      array.push string
+      parse text, array
+    end
   end
 end
--- csv_parser_spec.rb
+++ csv_parser_spec.rb
@@ -2,7 +2,37 @@ require_relative 'csv_parser'
 
 describe CSVParser do
   context "when given text has neither '\"' nor ',' as a part of one of its values" do
-    parsed = CSVParser.parse("a,b,c")
-    it { expect(parsed).to eq ['a', 'b', 'c'] }
+    context "when given text is 'a,b,c'" do
+      parsed= CSVParser.parse('a,b,c')
+      it { expect(parsed).to eq ['a', 'b', 'c'] }
+    end
+  end
+
+  context "when given text has ',' as a part of one of its values" do
+    context "when given text is '\"a,d\",b,c'" do
+      parsed= CSVParser.parse('"a,d",b,c')
+      it { expect(parsed).to eq ['a,d', 'b', 'c'] }
+    end
+  end
+
+  context "when given text has '\"' and ',' as a part of one of its values" do
+    context "when given text is '\"a,d\"\"\",b,c'" do
+      parsed= CSVParser.parse('"a,d""",b,c')
+      it { expect(parsed).to eq ['a,d"', 'b', 'c'] }
+    end
+  end
+
+  context "when given text has '\"' after ' ' as a part of one of its values" do
+    context "when given text is ' \"a\" ,b,c'" do
+      parsed= CSVParser.parse(' "a" ,b,c')
+      it { expect(parsed).to eq ['a', 'b', 'c'] }
+      end
+  end
+
+  context "when given text has '\"' after ' ' and has ',' as a part of one of its values" do
+    context "when given text is 'b,c, \"a\" ,\"b\",\"c \"'" do
+      parsed= CSVParser.parse('b,c, "a" ,"b","c "')
+      it { expect(parsed).to eq ['b', 'c', 'a', 'b', 'c '] }
+    end
   end
 end
